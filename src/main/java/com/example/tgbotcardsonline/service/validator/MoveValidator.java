8f1b0c2d37906725dafbf2061d4758c3d6e6fc45
Hlib Chibichian <chibgleb99@gmail.com>
2024-06-27 14:00:54 +0100

    refactored MoveValidator, created WinProcessor

--- src/main/java/com/example/tgbotcardsonline/service/validator/MoveValidator.java
+++ src/main/java/com/example/tgbotcardsonline/service/validator/MoveValidator.java
@@ -10,6 +10,7 @@ import com.example.tgbotcardsonline.repository.CardRepository;
 import com.example.tgbotcardsonline.repository.DeckResponseRepository;
 import com.example.tgbotcardsonline.repository.GameRepository;
 import com.example.tgbotcardsonline.repository.OnlinePlayerRepository;
+import com.example.tgbotcardsonline.service.processors.WinProcessor;
 import com.example.tgbotcardsonline.tg.TelegramBot;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,38 +26,24 @@ import static java.util.Objects.isNull;
 public class MoveValidator {
     private final DeckResponseRepository deckResponseRepository;
     private final TelegramBot telegramBot;
-    private final OnlinePlayerRepository onlinePlayerRepository;
-    private final CardRepository cardRepository;
-    private final GameRepository gameRepository;
+    private final WinProcessor winProcessor;
 
     public boolean isDefenceMoveValid(Game game, Card defendingCard) {
         Suit trumpSuit = game.getTrump();
         Card attackingCard = game.getOffensiveCard();
-
-        // If the defending card is of the same suit and has a higher rank
         if (attackingCard.getSuit().equals(defendingCard.getSuit()) &&
                 defendingCard.getValue().isHigherThan(attackingCard.getValue())) {
             return true;
         }
-
-        // If the defending card is a trump card and the attacking card is not a trump card
         if (defendingCard.getSuit().equals(trumpSuit) && !attackingCard.getSuit().equals(trumpSuit)) {
             return true;
         }
-
-        // If neither condition is met, the defense move is not valid
         return false;
     }
 
     public boolean isAttackMoveValid(Game game, Card playerMove) {
         List<Card> beatenCards = game.getBeaten();
-
-        // If there are no beaten cards, it's the first attack, which is always valid
-        if (beatenCards.isEmpty()) {
-            return true;
-        }
-
-        // Subsequent attacking move must match one of the ranks of the current attack
+        if(beatenCards.isEmpty())  return true;
         return beatenCards.stream().anyMatch(c -> c.getValue().equals(playerMove.getValue()));
     }
 
@@ -115,12 +102,7 @@ public class MoveValidator {
     }
 
     public String getPrettyMove(Card move) {
-        Map<String, String> suitSymbols = new HashMap<>();
-        suitSymbols.put("H", "♥");
-        suitSymbols.put("D", "♦");
-        suitSymbols.put("S", "♠");
-        suitSymbols.put("C", "♣");
-
+        Map<String, String> suitSymbols = getSuitSymbolsMap();
         String cardCode = move.getCode();
         String cardValue = cardCode.substring(0, cardCode.length() - 1);
         if (cardValue.equals("0")) cardValue = "10";
@@ -128,44 +110,25 @@ public class MoveValidator {
         return cardValue + suitSymbols.get(cardSuit);
     }
 
+    private static Map<String, String> getSuitSymbolsMap() {
+        Map<String, String> suitSymbols = new HashMap<>();
+        suitSymbols.put("H", "♥");
+        suitSymbols.put("D", "♦");
+        suitSymbols.put("S", "♠");
+        suitSymbols.put("C", "♣");
+        return suitSymbols;
+    }
+
     public boolean isPlayerWon(OnlinePlayer onlinePlayer) {
         Game game = onlinePlayer.getGame();
         DeckResponse deckResponse = deckResponseRepository.findByDeckId(game.getDeckId());
 
         if (onlinePlayer.getCards().isEmpty() && deckResponse.getRemaining() == 0){
-            processWinningState(onlinePlayer, game);
+            winProcessor.processWinningState(onlinePlayer, game);
             return true;
         }else {
             return false;
         }
     }
 
-    private void processWinningState(OnlinePlayer onlinePlayer, Game game) {
-        Player player = onlinePlayer.getPlayer();
-        OnlinePlayer attacker = game.getAttacker();
-        Player opponent = getOpponent(game, attacker, player);
-        processAppStatistics(game, player, opponent);
-        processDeletingDataFromDb(game);
-        notifyPlayersAboutFinishGame(game);
-    }
-
-    private static void processAppStatistics(Game game, Player player, Player opponent) {
-        player.setWins(player.getWins()+1);
-        opponent.setLosses(player.getLosses()+1);
-        game.setWinner(player);
-    }
-
-    private void processDeletingDataFromDb(Game game) {
-        game.setBeaten(null);
-        onlinePlayerRepository.deleteAll(List.of(game.getAttacker(),game.getDefender()));
-        deckResponseRepository.delete(deckResponseRepository.findByDeckId(game.getDeckId()));
-    }
-
-    private static Player getOpponent(Game game, OnlinePlayer attacker, Player player) {
-        return attacker.equals(player) ? game.getDefender().getPlayer() : attacker.getPlayer();
-    }
-
-    private void notifyPlayersAboutFinishGame(Game game){
-        telegramBot.sendMessageToBothPlayers(game,game.getWinner()+" won!");
-    }
 }
